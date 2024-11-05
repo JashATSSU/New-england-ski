e51c8c8dba91a57479450ddd9aa6af87295f35bf
JashATSSU <j_ash@salemstate.edu>
2024-11-05 12:54:16 -0500

    styling changes

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,6 +1,6 @@
 // src/components/navbar.js
 import React, { useEffect, useState } from "react";
-import { getUserInfo } from '../utilities/decodeJwt'; // Removed unused import
+import { getUserInfo } from '../utilities/decodeJwt';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import ReactNavbar from 'react-bootstrap/Navbar';
@@ -18,9 +18,10 @@ export default function Navbar() {
   return (
     <ReactNavbar bg="dark" variant="dark">
       <Container>
+        {/* Use the NavBar brand as a primary navigation link */}
+        <ReactNavbar.Brand href="/">New England Mountain Minder</ReactNavbar.Brand>
         <Nav className="me-auto">
-          {!isAuthenticated && <Nav.Link href="/">Start</Nav.Link>}
-          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/home">Homepage</Nav.Link>
           <Nav.Link href="/privateUserProfile">Profile</Nav.Link>
           <Nav.Link href="/searchSkiResorts">Ski Resorts</Nav.Link>
           <Nav.Link href="/MapSearch">Trail Maps</Nav.Link> 
